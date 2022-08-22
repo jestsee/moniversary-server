f36e4d80da1224c6da02e4285fb424728e68a0e4
Jesica <13519011@std.stei.itb.ac.id>
2022-08-22 20:51:37 +0700

    setup cors

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
-const compression = require('compression');
+const compression = require("compression");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(compression());
 
 const { getWishes, createWish, archiveWish } = require("./notion");
 
+var corsOptions = {
+  origin: "http://127.0.0.1:5173", // TODO ganti sm url deploy
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 app.get("/wishes", async (req, res) =>
   res.send({
+    state: "SUCCESS",
     message: "get all wishes",
     data: await getWishes(),
   })
 );
 app.post("/wish", async (req, res) =>
   res.send({
+    state: "SUCCESS",
     message: "new wish added",
     data: await createWish(req.body),
   })
